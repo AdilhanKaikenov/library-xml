3b5a8a7c873c7fcc979e8cdf3a3adeed1dab9b7f
AdilhanKaikenov <adilhan_kai@mail.ru>
2016-11-02 20:16:42 +0600

    Initial commit

---
+++ src/main/java/com/epam/adk/task3/library_xml/main/XMLApp.java
@@ -0,0 +1,18 @@
+package com.epam.adk.task3.library_xml.main;
+
+/**
+ * The class with main method. Created on 1.11.2016.
+ *
+ * @author Kaikenov Adilhan.
+ */
+public class XmlApp {
+
+    /**
+     * Application starting point.
+     *
+     * @param args input arguments array.
+     */
+    public static void main(String[] args) {
+
+    }
+}
