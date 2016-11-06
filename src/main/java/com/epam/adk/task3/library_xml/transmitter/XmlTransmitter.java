bf0c3c8ceb3413a06a9fb231bd5e732bd3e944bb
AdilhanKaikenov <adilhan_kai@mail.ru>
2016-11-06 13:57:52 +0600

    Add XmlTransmitter.java

---
+++ src/main/java/com/epam/adk/task3/library_xml/transmitter/XmlTransmitter.java
@@ -0,0 +1,12 @@
+package com.epam.adk.task3.library_xml.transmitter;
+
+/**
+ * The interface XmlTransmitter. Created on 06.11.2016.
+ *
+ * @author Kaikenov Adilhan.
+ */
+public interface XmlTransmitter {
+
+    void transterTo(String xmlFile, Object classForTransferring);
+
+}
