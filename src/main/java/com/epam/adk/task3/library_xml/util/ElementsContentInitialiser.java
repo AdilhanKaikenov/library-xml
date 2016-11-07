0b8d7af26cb4757cf5e0eb52bd164fad62a8b46c
AdilhanKaikenov <adilhan_kai@mail.ru>
2016-11-07 10:59:13 +0600

    ElementsContentInitialiser.java add reflection method

--- src/main/java/com/epam/adk/task3/library_xml/util/ElementsContentInitialiser.java
+++ src/main/java/com/epam/adk/task3/library_xml/util/ElementsContentInitialiser.java
@@ -9,6 +9,9 @@ import com.epam.adk.task3.library_xml.parser.enums.ElementEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.time.Year;
 
 /**
@@ -20,15 +23,65 @@ public class ElementsContentInitialiser {
 
     private static final Logger log = LoggerFactory.getLogger(ElementsContentInitialiser.class);
 
+    //TODO:
+    public static void universalInitialize(ElementEnum elementEnum, Book book, Authors authors, String content) {
+        log.trace("Entering ElementsContentInitialiser class, universalInitialize( Argument: elementEnum = {}) method", elementEnum);
+
+        if (elementEnum != null) {
+        String element = elementEnum.getValue();
+
+            Class<? extends Book> bookClass = book.getClass();
+            authors = new Authors();
+            Class<? extends Authors> authorsClass = authors.getClass();
+
+            Field[] bookClassFields = bookClass.getDeclaredFields();
+            Method[] bookClassMethods = bookClass.getMethods();
+
+            Field[] authorsClassFields = authorsClass.getDeclaredFields();
+            Method[] authorsClassMethods = authorsClass.getMethods();
+
+            Author author = null;
+
+            try {
+                for (Field field : bookClassFields) {
+                    for (Method method : bookClassMethods) {
+                        if (field.getName().equals(element)
+                                && method.getName().toLowerCase().contains(field.getName())
+                                && method.getName().contains("set")) {
+                            if (field.getName().equals("authors")) {
+
+                                for (Field authorsField : authorsClassFields) {
+                                    for (Method authorsMethod : authorsClassMethods) {
+                                        if (authorsField.getName().equals(element)
+                                                && authorsMethod.getName().toLowerCase().contains(authorsField.getName())
+                                                && authorsMethod.getName().contains("set")) {
+                                            author = new Author();
+                                            author.setAuthor(content);
+                                            authors.add(author);
+                                        }
+                                    }
+                                }
+                            }
+                            method.invoke(book, content); // TODO: String content --> IllegalArgumentException
+                        }
+                    }
+                }
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                log.error("");
+            }
+        }
+        log.trace("ElementsContentInitialiser class, universalInitialize() method finished!");
+    }
+
     /**
      * The method for initializing entities on the current element (tag).
      *
      * @param elementEnum element enumeration.
-     * @param book Book instance.
-     * @param authors Authors instance.
-     * @param content tag text.
+     * @param book        Book instance.
+     * @param authors     Authors instance.
+     * @param content     tag text.
      */
-    public static void initialize(ElementEnum elementEnum, Book book, Authors authors, String content){
+    public static void initialize(ElementEnum elementEnum, Book book, Authors authors, String content) {
         log.trace("Entering ElementsContentInitialiser class, initialize( Argument: elementEnum = {}) method", elementEnum);
         if (elementEnum != null && book != null) {
             switch (elementEnum) {
